0b6753a4d33c46b0cdd9d36adeab068f0aa2276b
Rafael Silva <rafaeldocarmo.silva1995@gmail.com>
2026-05-04 20:43:09 -0300

    Acrescentado tabela comprovando a maior amostragem nos veículos que possuem um maior km

--- Preco_medio_km.sql
+++ Preco_medio_km.sql
@@ -6,7 +6,8 @@ SELECT
         WHEN Mileage BETWEEN 20000 AND 60000 THEN '20k-60k km (Média)'
         ELSE 'Acima de 60k km (Alta)'
     END AS Quilometragem,
-    FORMAT(AVG(TRY_CONVERT(DECIMAL(10,2), PRICE)), 'C', 'PT-BR') AS [Preco MEDIO]
+    FORMAT(AVG(TRY_CONVERT(DECIMAL(10,2), PRICE)), 'C', 'PT-BR') AS [Preco MEDIO],
+	COUNT([Car ID]) AS QTD
 FROM car_price_prediction_
 GROUP BY 
     CASE 
